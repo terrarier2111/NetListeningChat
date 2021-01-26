c9408b2484f3bc9c30877234bac934c69b889169
terrarier2111 <hannes.gaumann@outlook.de>
2021-01-26 17:34:20 +0100

    Initial commit

---
+++ src/main/java/de/terrarier/nlchat/NLChatClient.java
@@ -0,0 +1,64 @@
+package de.terrarier.nlchat;
+
+import de.terrarier.netlistening.Client;
+import de.terrarier.netlistening.api.DataContainer;
+import de.terrarier.netlistening.api.event.*;
+
+public class NLChatClient {
+
+    public static void main(String[] args) {
+        final Client client = new Client.Builder("localhost", 6790)
+                .timeout(15000)
+                .build();
+
+        client.registerListener(new DecodeListener() {
+            @Override
+            public void trigger(DecodeEvent decodeEvent) {
+                final String message = decodeEvent.getData().read();
+                System.out.println("Server: " + message);
+            }
+        });
+
+        client.registerListener(new ConnectionPostInitListener() {
+            @Override
+            public void trigger(ConnectionPostInitEvent connectionPostInitEvent) {
+                System.out.println("The connection connected!");
+            }
+        });
+
+        client.registerListener(new ConnectionDisconnectListener() {
+            @Override
+            public void trigger(ConnectionDisconnectEvent connectionDisconnectEvent) {
+                System.out.println("The connection disconnected!");
+            }
+        });
+
+        client.registerListener(new ConnectionTimeoutListener() {
+            @Override
+            public void trigger(ConnectionTimeoutEvent connectionTimeoutEvent) {
+                System.out.println("The connection timed out!");
+            }
+        });
+
+        final NLChatClient chatClient = new NLChatClient(client);
+        chatClient.sendMessage("Hello, guys!"); // this gets sent when the connection was established
+        // Do other stuff with the client
+    }
+
+    private final Client client;
+
+    public NLChatClient(Client client) {
+        this.client = client;
+    }
+
+    public void sendMessage(String message) {
+        final DataContainer data = new DataContainer();
+        data.add(message);
+        client.sendData(data);
+    }
+
+    public void stop() {
+        client.stop();
+    }
+
+}
---
+++ src/main/java/de/terrarier/nlchat/NLChatServer.java
@@ -0,0 +1,71 @@
+package de.terrarier.nlchat;
+
+import de.terrarier.netlistening.Server;
+import de.terrarier.netlistening.api.DataContainer;
+import de.terrarier.netlistening.api.event.*;
+
+public class NLChatServer {
+
+    public static void main(String[] args) {
+        final Server server = new Server.Builder(6790)
+                .timeout(15000)                                                        // timeout
+                .encryption().build()                                                  // encryption
+                .compression().nibbleCompression(true).varIntCompression(true).build() // compression
+                .build();
+
+        server.registerListener(new DecodeListener() {
+            @Override
+            public void trigger(DecodeEvent decodeEvent) {
+                final String message = decodeEvent.getData().read();
+                System.out.println(decodeEvent.getConnection().getId() + ": " + message);
+            }
+        });
+
+        server.registerListener(new ConnectionPostInitListener() {
+            @Override
+            public void trigger(ConnectionPostInitEvent connectionPostInitEvent) {
+                System.out.println(connectionPostInitEvent.getConnection().getId() + " connected!");
+            }
+        });
+
+        server.registerListener(new ConnectionDisconnectListener() {
+            @Override
+            public void trigger(ConnectionDisconnectEvent connectionDisconnectEvent) {
+                System.out.println(connectionDisconnectEvent.getConnection().getId() + " disconnected!");
+            }
+        });
+
+        server.registerListener(new ConnectionTimeoutListener() {
+            @Override
+            public void trigger(ConnectionTimeoutEvent connectionTimeoutEvent) {
+                System.out.println(connectionTimeoutEvent.getConnection().getId() + " timed out!");
+            }
+        });
+
+        final NLChatServer chatServer = new NLChatServer(server);
+        // Do other stuff with the server
+    }
+
+    private final Server server;
+
+    public NLChatServer(Server server) {
+        this.server = server;
+    }
+
+    public void sendMessage(String message) {
+        final DataContainer data = new DataContainer();
+        data.add(message);
+        server.sendData(data);
+    }
+
+    public void sendMessage(String message, int target) {
+        final DataContainer data = new DataContainer();
+        data.add(message);
+        server.sendData(data, target);
+    }
+
+    public void stop() {
+        server.stop();
+    }
+
+}
